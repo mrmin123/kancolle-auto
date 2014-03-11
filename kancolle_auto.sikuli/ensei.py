e2e6d979caadf1b775c61985d17fd4ad5a6e9980
amylase <amylase.inquiry@gmail.com>
2014-03-11 15:41:46 +0900

    added new ensei.

--- kancolle_auto.sikuli/ensei.py
+++ kancolle_auto.sikuli/ensei.py
@@ -35,6 +35,40 @@ def ensei_factory(ensei_id):
     if ensei_id == 4: return Ensei(4, 'ensei_name_04.png', 'ensei_area_01.png', datetime.timedelta(minutes = 50))
     if ensei_id == 5: return Ensei(5, 'ensei_name_05.png', 'ensei_area_01.png', datetime.timedelta(minutes = 90))
     if ensei_id == 6: return Ensei(6, 'ensei_name_06.png', 'ensei_area_01.png', datetime.timedelta(minutes = 40))
-    if ensei_id == 20: return Ensei(20, 'ensei_name_20.png', 'ensei_area_03.png', datetime.timedelta(minutes = 120))
+    if ensei_id == 7: return Ensei(7, 'ensei_name_07.png', 'ensei_area_01.png', datetime.timedelta(housr = 1))
+    if ensei_id == 8: return Ensei(8, 'ensei_name_08.png', 'ensei_area_01.png', datetime.timedelta(hours = 3))
+
+    if ensei_id == 9: return Ensei(9, 'ensei_name_09.png', 'ensei_area_02.png', datetime.timedelta(hours = 4))
+    if ensei_id == 10: return Ensei(10, 'ensei_name_10.png', 'ensei_area_02.png', datetime.timedelta(hours = 1, minutes = 30))
+    if ensei_id == 11: return Ensei(11, 'ensei_name_11.png', 'ensei_area_02.png', datetime.timedelta(hours = 5))
+    if ensei_id == 12: return Ensei(12, 'ensei_name_12.png', 'ensei_area_02.png', datetime.timedelta(hours = 8))
+    if ensei_id == 13: return Ensei(13, 'ensei_name_13.png', 'ensei_area_02.png', datetime.timedelta(hours = 4))
+    if ensei_id == 14: return Ensei(14, 'ensei_name_14.png', 'ensei_area_02.png', datetime.timedelta(hours = 6))
+    if ensei_id == 15: return Ensei(15, 'ensei_name_15.png', 'ensei_area_02.png', datetime.timedelta(hours = 12))
+    if ensei_id == 16: return Ensei(16, 'ensei_name_16.png', 'ensei_area_02.png', datetime.timedelta(hours = 15))
+
+    if ensei_id == 17: return Ensei(17, 'ensei_name_17.png', 'ensei_area_03.png', datetime.timedelta(minutes = 45))
+    if ensei_id == 18: return Ensei(18, 'ensei_name_18.png', 'ensei_area_03.png', datetime.timedelta(hours = 5))
+    if ensei_id == 19: return Ensei(19, 'ensei_name_19.png', 'ensei_area_03.png', datetime.timedelta(hours = 6))
+    if ensei_id == 20: return Ensei(20, 'ensei_name_20.png', 'ensei_area_03.png', datetime.timedelta(hours = 2))
+    if ensei_id == 21: return Ensei(21, 'ensei_name_21.png', 'ensei_area_03.png', datetime.timedelta(hours = 2, minutes = 20))
+    if ensei_id == 22: return Ensei(22, 'ensei_name_22.png', 'ensei_area_03.png', datetime.timedelta(hours = 3))
+
+    if ensei_id == 25: return Ensei(25, 'ensei_name_25.png', 'ensei_area_04.png', datetime.timedelta(hours = 40))
+    if ensei_id == 26: return Ensei(26, 'ensei_name_26.png', 'ensei_area_04.png', datetime.timedelta(hours = 80))
+    if ensei_id == 27: return Ensei(27, 'ensei_name_27.png', 'ensei_area_04.png', datetime.timedelta(hours = 20))
+    if ensei_id == 28: return Ensei(28, 'ensei_name_28.png', 'ensei_area_04.png', datetime.timedelta(hours = 25))
+    if ensei_id == 29: return Ensei(29, 'ensei_name_29.png', 'ensei_area_04.png', datetime.timedelta(hours = 24))
+    if ensei_id == 30: return Ensei(30, 'ensei_name_30.png', 'ensei_area_04.png', datetime.timedelta(hours = 48))
+
+    if ensei_id == 35: return Ensei(35, 'ensei_name_35.png', 'ensei_area_05.png', datetime.timedelta(hours = 7))
+    if ensei_id == 36: return Ensei(36, 'ensei_name_36.png', 'ensei_area_05.png', datetime.timedelta(hours = 9))
+    if ensei_id == 37: return Ensei(37, 'ensei_name_37.png', 'ensei_area_05.png', datetime.timedelta(hours = 2, minutes = 45))
     return ensei_factory(2)
               
+
+
+
+
+
+
