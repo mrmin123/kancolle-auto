075c571cb3d8e5e3de45571127112058890155e0
Minyoung Choi <minyoung.choi@gmail.com>
2015-11-12 23:29:02 -0800

    add images; start dev on quests

--- kancolle_auto.sikuli/quests.sikuli/quests.py
+++ kancolle_auto.sikuli/quests.sikuli/quests.py
@@ -6,6 +6,8 @@ from util import *
 class Quests:
     def __init__(self, kc_window, settings):
         self.kc_window = kc_window
+        self.active_quests = ['d2', 'd3', 'd4', 'd9'].sort()
+        self.first_type = self.active_quests[0][0]
 
     def go_quests(self):
         # Navigate to Expedition menu
@@ -14,4 +16,9 @@ class Quests:
         sleep(2)
 
     def check_quests(self):
-        pass
+        while not self.kc_window.exists(self.first_type + '.png'):
+            if check_and_click(self.kc_window, Pattern('quests_next_page.png').exact()):
+                pass
+            else:
+                print 'Quests not found!'
+
