42c6f1cd5a3650a9e7268789b4cad35edbb39c78
Minyoung Choi <minyoung.choi@gmail.com>
2015-11-12 23:43:37 -0800

    basic implementation

--- kancolle_auto.sikuli/quests.sikuli/quests.py
+++ kancolle_auto.sikuli/quests.sikuli/quests.py
@@ -7,7 +7,9 @@ class Quests:
     def __init__(self, kc_window, settings):
         self.kc_window = kc_window
         self.active_quests = ['d2', 'd3', 'd4', 'd9'].sort()
+        self.active_quests_unchecked = ['d2', 'd3', 'd4', 'd9'].sort()
         self.first_type = self.active_quests[0][0]
+        self.last_type = self.active_quests[-1][0]
 
     def go_quests(self):
         # Navigate to Expedition menu
@@ -16,9 +18,21 @@ class Quests:
         sleep(2)
 
     def check_quests(self):
+        start_check = True
         while not self.kc_window.exists(self.first_type + '.png'):
             if check_and_click(self.kc_window, Pattern('quests_next_page.png').exact()):
                 pass
             else:
                 print 'Quests not found!'
+                start_check = False
+                break
+
+        while start_check:
+            for quest in self.active_quests_unchecked:
+                if check_and_click(self.kc_window, Pattern(quest + '.png').exact()):
+                    self.active_quests_unchecked.remove(quest)
+            if check_and_click(self.kc_window, Pattern('quests_next_page.png').exact()):
+                pass
+            else:
+                start_check = False
 
