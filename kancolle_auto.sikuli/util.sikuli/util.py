34bffa9b478c5fadc7b4df182d4ab2f4372d73c1
Minyoung Choi <minyoung.choi@gmail.com>
2016-01-02 00:10:32 -0800

    tweak expanded click areas

--- kancolle_auto.sikuli/util.sikuli/util.py
+++ kancolle_auto.sikuli/util.sikuli/util.py
@@ -147,7 +147,7 @@ def expand_areas(target):
     elif target == 'quests_navigation':
         return [-10, 10, -5, 5]
     elif target == 'quest_completed':
-        return [-580, 25, 25, -25]
+        return [-580, 25, -25, 25]
     elif target == 'pvp_row':
         return [-500, 50, -10, 40]
 
