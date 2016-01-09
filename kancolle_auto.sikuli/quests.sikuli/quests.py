125103e017e6a590702b83045e2a03dde05fc1d2
Minyoung Choi <minyoung.choi@gmail.com>
2016-01-09 14:37:37 -0800

    update bw6, bw7; enable in quests module

--- kancolle_auto.sikuli/quests.sikuli/quests.py
+++ kancolle_auto.sikuli/quests.sikuli/quests.py
@@ -13,6 +13,7 @@ class Quests:
         self.combat_enabled = settings['combat_enabled']
         if self.combat_enabled:
             self.combat_area = settings['combat_area']
+            self.combat_subarea = settings['combat_subarea']
         self.pvp_enabled = settings['pvp_enabled']
         self.expeditions_enabled = settings['expeditions_enabled']
         self.quests_checklist = list(settings['active_quests'])
@@ -180,14 +181,14 @@ class Quests:
                             self.quest_tree.add_children('bd5', [QuestNode('bw2', [5, 0, 0])])
                             if 'bw5' in self.quests_checklist:
                                 self.quest_tree.add_children('bw2', [QuestNode('bw5', [5, 0, 0])])
-                        #        if 'bw6' in self.quests_checklist:
-                        #            self.quest_tree.add_children('bw5', [QuestNode('bw6', [12, 0, 0])])
+                                if 'bw6' in self.quests_checklist and self.combat_area == 4:
+                                    self.quest_tree.add_children('bw5', [QuestNode('bw6', [12, 0, 0])])
                         #            if 'bw8' in self.quests_checklist:
                         #                self.quest_tree.add_children('bw6', [QuestNode('bw8', [1, 0, 0])])
                         #                if 'bw9' in self.quests_checklist:
                         #                    self.quest_tree.add_children('bw8', [QuestNode('bw9', [2, 0, 0])])
-                        #        if 'bw7' in self.quests_checklist:
-                        #                self.quest_tree.add_children('bw5', [QuestNode('bw7', [5, 0, 0])])
+                                if 'bw7' in self.quests_checklist and self.combat_area == 3 and (self.combat_subarea == 3 or self.combat_subarea == 4 or self.combat_subarea == 5):
+                                        self.quest_tree.add_children('bw5', [QuestNode('bw7', [5, 0, 0])])
                     if 'bw1' in self.quests_checklist:
                         self.quest_tree.add_children('bd2', [QuestNode('bw1', [12, 0, 0])])
                         if 'bw4' in self.quests_checklist:
