89ff37aa5850535a980de6a66bd2bc391820612b
Minyoung Choi <minyoung.choi@gmail.com>
2016-02-16 14:05:44 -0800

    bugfix

--- kancolle_auto.sikuli/combat.sikuli/combat.py
+++ kancolle_auto.sikuli/combat.sikuli/combat.py
@@ -229,7 +229,6 @@ class Combat:
         while not loop_pre_combat_stop:
             # If compass, press it
             #if self.kc_window.exists(pattern_generator(self.kc_window, Pattern('compass.png'), expand_areas('compass'))):
-            print 'check compass'
             if check_and_click(self.kc_window, 'compass.png', expand_areas('compass')):
                 # Rework for new resupply screen
                 self.kc_window.click(self.kc_window.getLastMatch())
@@ -242,7 +241,6 @@ class Combat:
                 loop_pre_combat_stop = True
                 break
             # Node select
-            print 'check node select'
             elif len(self.node_selects) > 0 and self.kc_window.exists('combat_node_select.png'):
                 for node in self.node_selects:
                     check_and_click(self.kc_window, Pattern('%s.png' % node), expand_areas('node_select'))
@@ -253,7 +251,6 @@ class Combat:
                 break
             # If formation select, select formation based on user config
             #if self.kc_window.exists(pattern_generator(self.kc_window, Pattern('formation_%s.png' % self.formations[nodes_run]).similar(0.95))):
-            print 'check formation %s' % self.formations[nodes_run]
             elif check_and_click(self.kc_window, Pattern('formation_%s.png' % self.formations[nodes_run]).similar(0.95)):
                 # Now check for night battle prompt or post-battle report
                 log_msg("Selecting fleet formation!")
