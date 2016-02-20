74bc54a6bbc6dbbe8db5210c6d13bdf595f3fcea
Minyoung Choi <minyoung.choi@gmail.com>
2016-02-20 14:19:22 -0800

    ensure only one fleet can return per check_expedition

--- kancolle_auto.sikuli/kancolle_auto.py
+++ kancolle_auto.sikuli/kancolle_auto.py
@@ -99,6 +99,7 @@ def check_expedition():
                 if now_time > expedition.end_time and not expedition.returned:
                     fleet_needs_resupply[expedition.fleet_id - 1] = True
                     expedition.returned = True
+                    break
         # Let the Quests module know, if it's enabled
         if settings['quests_enabled'] == True:
             quest_item.done_expeditions += 1
