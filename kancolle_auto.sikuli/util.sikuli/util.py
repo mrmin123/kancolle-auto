20e692a832bf4e21030ce27fc5dc10c768b7a8b4
Minyoung Choi <minyoung.choi@gmail.com>
2016-03-11 18:39:02 -0800

    tighten up sleeps

--- kancolle_auto.sikuli/util.sikuli/util.py
+++ kancolle_auto.sikuli/util.sikuli/util.py
@@ -400,7 +400,7 @@ def rnavigation(kc_window, destination, max=0):
         elif final_target in ['menu_top_quests.png']:
             if kc_window.exists('quests_screen_check.png'):
                 wait_and_click(kc_window, 'quests_screen_check.png', expand=expand_areas('quests_screen_check')) # Go away Ooyodo
-                sleep(1)
+                sleep_fast()
                 final_target = ''
         else:
             final_target = ''
